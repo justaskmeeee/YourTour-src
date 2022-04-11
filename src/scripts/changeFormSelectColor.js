b0660135b0a409c545584e6f162733a17634ac88
justaskmeeee <yunmyi@icloud.com>
2022-04-11 04:38:12 +0300

    Added input date type and comments for form

--- src/scripts/changeFormSelectColor.js
+++
@@ -1,9 +0,0 @@
-function changeFormSelectColor(event) {
-  const formSelect = document.querySelector('.tour-form__select');
-  
-  formSelect.addEventListener('change', () => {
-    formSelect.classList.add('tour-form__select_black');
-  })  
-}
-
-changeFormSelectColor();
