1dd0edd8f0bb90e52e874a22fa302162d92cc27b
justaskmeeee <102696559+justaskmeeee@users.noreply.github.com>
2022-06-25 05:21:58 +0300

    merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -47,6 +48,15 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/favicon'),
+          to: path.resolve(__dirname, './dist/favicon')
+
+        }
+      ]
+    }),
   ],
   // mode: 'development',
   mode: 'production',
